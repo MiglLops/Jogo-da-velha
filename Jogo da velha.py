f6f70c380d52a3e750690d9853b5a0178d53ed05
Miguel Lopes <migueldossantoslopes125@gmail.com>
2025-10-04 21:44:40 -0300

    Add files via upload

--- Jogo da velha.py
+++ Jogo da velha.py
@@ -1,28 +1,52 @@
 #Jogo da velha - inicio 24/09/2025
+def jogar():
+    lista_print = ["[0]", "[1]", "[2]", "[3]",
+            "[4]", "[5]", "[6]",
+            "[7]", "[8]", "[9]"]
 
-lista_print = ["[0]", "[1]", "[2]", "[3]",
-         "[4]", "[5]", "[6]",
-         "[7]", "[8]", "[9]"]
+    def grades():
+        print(lista_print[1], lista_print[2], lista_print[3])
+        print(lista_print[4], lista_print[5], lista_print[6])
+        print(lista_print[7], lista_print[8], lista_print[9])
+        print()
 
-jogando = True
-while jogando == True:
-    print(lista_print[1], lista_print[2], lista_print[3])
-    print(lista_print[4], lista_print[5], lista_print[6])
-    print(lista_print[7], lista_print[8], lista_print[9])
+    def turnos():
+        escolha = int(input("Escolha a casa para jogar 'X':"))
+        pop = lista_print.pop(escolha)
+        X = lista_print.insert(escolha, "X") 
+        grades()
+        detectar_venceu()
+        escolha = int(input("Escolha a casa para jogar 'O':"))
+        pop = lista_print.pop(escolha)
+        O = lista_print.insert(escolha, "O") 
 
-    escolha = int(input("Escolha a casa para jogar 'X':"))
-    pop = lista_print.pop(escolha)
-    X = lista_print.insert(escolha, "X")
+    def detectar_venceu():
+        combinacoes = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],  
+            [1, 4, 7],  
+            [2, 5, 8],  
+            [3, 6, 9],  
+            [1, 5, 9],  
+            [3, 5, 7]   
+        ]
+        for a, b, c in combinacoes:
+            if lista_print[a] == lista_print[b] == lista_print[c]:
+                print("FIM - Jogador", lista_print[a], "venceu!")
+                grades()
+                novamente = input("Deseja jogar novamente? S/n: ")
+                if novamente == "n":
+                    exit()
+                else:
+                    jogar()
 
-    #pra ve se ganhou
+    jogando = True
+    while jogando == True:
+        grades()
+        turnos()
+        detectar_venceu()
 
-    if lista_print[2] and lista_print[5] and lista_print[8] == "X":
-        print("venceu")
-    elif lista_print[1] and lista_print[5] and lista_print[9] == "X":
-        print("venceu")
-    elif lista_print[3] and lista_print[5] and lista_print[7] == "X":
-        print("venceu")
-    elif lista_print[1] and lista_print[4] and lista_print[7] == "X":
-        print("venceu")
-    elif lista_print[3] and lista_print[6] and lista_print[9] == "X":
-        print("venceu")
+jogar()
+
+
